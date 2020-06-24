179f011f305d13d7474d5786df4e6f34e078213a
Kyle <obrienk@webbhost.net>
2020-06-24 16:31:45 -0500

    added store info page

---
+++ models/Store.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const StoreSchema = new Schema({
+  _id: ObjectId,
+  id: String,
+  domain: String,
+  secure_url: String,
+  control_panel_base_url: String,
+  status: String,
+  name: String,
+  first_name: String,
+  last_name: String,
+  address: String,
+  country: String,
+  country_code: String,
+  phone: Number,
+  admin_email: String,
+  order_email: String,
+  favicon_url: String,
+  timezone: {
+    name: String,
+    raw_offset: Number,
+    dst_offset: Number,
+    dst_correction: Boolean,
+    date_format: {
+      diplay: String,
+      export: String,
+      extended_display: String
+    }
+  },
+    language: String,
+    currency: String,
+    currency_symbol: String,
+    decimal_separator: String,
+    thousands_separator: String,
+    decmial_places: Number,
+    currency_symbol_location: String,
+    weight_units: String,
+    dimension_units: String,
+    dimension_decimal_places: Number,
+    dimension_decimal_token: String,
+    plan_name: String
+  }
+);
+
+module.exports = mongoose.model('Store', StoreSchema);
---
+++ routes/Store.js
@@ -0,0 +1,78 @@
+require('dotenv').config();
+// Change to base branch test
+const express = require('express');
+const router = express.Router();
+const Store = require('../models/Store.js');
+BigCommerce = require('node-bigcommerce');
+
+
+const bigCommerce = new BigCommerce({
+  logLevel: 'info',
+  clientId: process.env.CLIENT,
+  accessToken: process.env.TOKEN,
+  secret: process.env.SECRET,
+  storeHash: process.env.HASH,
+  responseType: 'json',
+  apiVersion: 'v2' // Default is v2
+});
+router.get('/store', (req, res) => {
+  Store.find({}, (err, allStores) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/');
+    } else {
+      res.render('index', {
+        title: 'MVC Example | View Store Info',
+        Stores: allStores
+      });
+    }
+  });
+});
+router.get('/store/:storeId', (req, res) => {
+  let storeId = req.params.storeId;
+
+  Store.findOne({storeId: storeId}).exec((err, ret) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.render('store', {
+      title: 'Store Details',
+      Store: ret,
+      message: ''
+    });
+  });
+})
+router.get('/store/update', (req, res) => {
+  res.render('index', { message: 'Updating' });
+  const getStoreInfo = new Promise(async function() {
+    try{
+     bigCommerce.get('/store').then(data => {
+        console.log('RUNNING...')
+        const context = {}
+                context.data = data;
+                const newStore = {}
+                newStore.storeId = context.data.id;
+                newStore.domain = context.data.domain;
+                newStore.secure_url = context.data.secure_url;
+                newStore.control_panel_base_url = context.data.base_url;
+                newStore.status = context.data.status;
+                newStore.name = context.data.name;
+
+                console.log(newStore);
+                Store.collection.insertOne(newStore , function(err, res) {
+                  if (err) throw err;
+                  console.log(
+                    'Number of documents inserted: ' + res.insertedCount
+                  );
+                  });
+                })
+              }
+      catch(err){
+        console.log(err + 'line 71 Store.js');
+        return err;
+      }
+  });
+  getStoreInfo;
+});
+
+module.exports = router;
